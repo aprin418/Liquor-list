d18f787aa9be38f44d591403f8f8a561deba1955
aprin418 <aprin418@gmail.com>
2021-08-16 15:00:03 -0400

    update login routes

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(layouts);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 app.use("/auth", require("./controllers/auth"));
